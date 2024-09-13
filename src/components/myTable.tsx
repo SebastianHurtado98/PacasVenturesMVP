9d792a49f78e9d6c9cd754a46ebdcb24668a122e
SebastianHurtado98 <sebastian.hurtado@utec.edu.pe>
2024-09-13 17:37:34 -0500

    start project

---
+++ src/components/myTable.tsx
@@ -0,0 +1,36 @@
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
+export default function Component() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Factura</TableHead>
+          <TableHead>Estado</TableHead>
+          <TableHead>Método</TableHead>
+          <TableHead className="text-right">Monto</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">FAC001</TableCell>
+          <TableCell>Pagado</TableCell>
+          <TableCell>Tarjeta de Crédito</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">FAC002</TableCell>
+          <TableCell>Pendiente</TableCell>
+          <TableCell>PayPal</TableCell>
+          <TableCell className="text-right">$150.00</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">FAC003</TableCell>
+          <TableCell>No Pagado</TableCell>
+          <TableCell>Transferencia Bancaria</TableCell>
+          <TableCell className="text-right">$350.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
