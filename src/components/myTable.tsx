c0b3412decce52136c850a71a3f7b545d9479487
SebastianHurtado98 <sebastian.hurtado@utec.edu.pe>
2024-09-13 17:50:13 -0500

    Fix

--- src/components/myTable.tsx
+++ src/components/myTable.tsx
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 export default function Component() {
   return (
